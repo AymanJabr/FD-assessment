b8e13dec74c92451ea665b36b3367641bccdd057
AymanJabr <aymanjaber2012@gmail.com>
2026-01-06 20:00:20 +0300

    feat: initial pseudocode ideation

--- Algorithmic task/README.md
+++ Algorithmic task/README.md
@@ -16,4 +16,13 @@ node index.js
 
 
 ## TODO
+-We should have another file with a series of test inputs, the test inputs are going to be:
+1) the standard empty array
+2) an array where no outgoing from "you" let's to "out"
+3) an array, where you is the initial location for all of them, and all the outputs are going to end in "out", with several layers in the middle of connections
+4) an array with no "you", and another with no "out".
+5) A circular test case, where out just also leads to other inputs in the beginning.
+
+6) The input example
+7) The actual question that was given to us to solve
 Maybe also Dockerize this once you are done
--- Algorithmic task/index.js
+++ Algorithmic task/index.js
@@ -1,1 +1,48 @@
+// Input Example:
+
+// aaa: you hhh
+// you: bbb ccc
+// bbb: ddd eee
+// ccc: ddd eee fff
+// ddd: ggg iii
+// eee: out
+// fff: out
+// ggg: out
+// hhh: ccc fff iii
+// iii: out
+
+//  1) Let's create a Device Object
+class Devices {
+
+    id;
+    outputs;
+
+    constructor(id) {
+        this.id = id;
+        this.outputs = [];
+    }
+}
+
+// 2) We probably need to convert the input in the format given in the example, to Devices
+
+
+// 3) 
+// Now we will need 3 arrays, and a counter:
+// - queue: to determine the next device to visit
+// - visitedDevices: to keep track of visited devices
+// - successfulDevices: In our example, we know that device "fff" can get us to "out", so any new path that leads to "fff" is a successful path
+let queue = ["you"];
+let visitedDevices = [];
+let successfulDevices = ["out"];
+
+let pathCount = 0;
+
+// 4) This is where we start with the actual algorithm, we are going to start with the node "you" in the queue, we are going to:
+// 1 => Take/Remove the first element of the queue, and add it to the visitedDevices array
+// 2 => For it's outputs, we are going to go output by output
+//    2.1 => we are going to check if any of them is in the successfulDevices array, if so, we are going to increment the pathCount, and add the current node to the successfulDevices Array
+//    2.2 => If not, we are going check the visitedDevices array, if it's in this array we are going to skip this output
+//    2.3 => If not, we are going to add it to the queue
+// 3 => Once the queue is empty, we are going to return the pathCount
+
 console.log("hello world");
